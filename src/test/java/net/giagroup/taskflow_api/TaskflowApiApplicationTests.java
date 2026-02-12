ad78ca1677545a8e1388462a8633fed00822f1a6
KengniJohan <kengnijordan9@gmail.com>
2026-02-12 22:14:55 +0100

    Init project

---
+++ src/test/java/net/giagroup/taskflow_api/TaskflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.giagroup.taskflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
